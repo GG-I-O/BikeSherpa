66abe22df903058c6f93f2a1767da1c8e72b1859
aepiard101010 <a.epiard.101010@gmail.com>
2025-12-15 14:38:50 +0100

    Removed unused imports

--- sources/gui/Ggio.BikeSherpa.Frontend/app/login.tsx
+++ sources/gui/Ggio.BikeSherpa.Frontend/app/login.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 import { Text, View } from "react-native";
 import { useAuth0 } from "react-native-auth0";
 import { Button } from "react-native-paper";
