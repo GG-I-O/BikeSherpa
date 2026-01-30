0226c459a0936afb76daa4c2fc9f51c05e4ceb10
francois_popieul <francois.popieul@hotmail.fr>
2026-01-30 14:16:34 +0100

    Updated a zod schema

--- sources/gui/Ggio.BikeSherpa.Frontend/customers/viewModels/EditCustomerFormViewModel.ts
+++ sources/gui/Ggio.BikeSherpa.Frontend/customers/viewModels/EditCustomerFormViewModel.ts
@@ -21,29 +21,25 @@ export default class EditCustomerFormViewModel {
 
     public getEditCustomerSchema(customerToEdit: Customer, customerList: Customer[]) {
         const originalCode = customerToEdit.code;
-        const newCustomerViewModel = new NewCustomerFormViewModel(this.customerServices);
-        const getEditCustomerSchema = newCustomerViewModel.getNewCustomerSchema(customerList);
-        return zod.object({
-            id: zod
-                .string()
-                .min(1),
-            code: zod
-                .string()
-                .trim()
-                .min(1, "Code requis")
-                .max(3, "Code trop long")
-                .refine((value) => {
-                    if (originalCode === value) {
-                        return true;
-                    }
-                    return !customerList.some((customer) => customer.code === value);
-                }, "Le code doit être unique"),
-            name: getEditCustomerSchema.shape.name,
-            address: getEditCustomerSchema.shape.address,
-            vatNumer: getEditCustomerSchema.shape.vatNumber,
-            siret: getEditCustomerSchema.shape.siret,
-            email: getEditCustomerSchema.shape.email,
-            phoneNumber: getEditCustomerSchema.shape.phoneNumber
-        })
+        const newCustomerSchema = new NewCustomerFormViewModel(this.customerServices).getNewCustomerSchema(customerList);
+
+        return newCustomerSchema
+            .partial({ siret: true, vatNumber: true })
+            .extend({
+                id: zod
+                    .string()
+                    .min(1),
+                code: zod
+                    .string()
+                    .trim()
+                    .min(1, "Code requis")
+                    .max(3, "Code trop long")
+                    .refine((value) => {
+                        if (originalCode === value) {
+                            return true;
+                        }
+                        return !customerList.some((customer) => customer.code === value);
+                    }, "Le code doit être unique"),
+            });
     }
 }
