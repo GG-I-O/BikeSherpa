c9b1d0e7059766cce70b5d70efb46c925e208a7f
francois_popieul <francois.popieul@hotmail.fr>
2026-01-30 11:12:31 +0100

    Migration and database update

--- sources/core/Ggio.BikeSherpa.Backend.Infrastructure/Migrations/20260130100653_UpdateCourierEntity.Designer.cs
+++ sources/core/Ggio.BikeSherpa.Backend.Infrastructure/Migrations/20260130100653_UpdateCourierEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Ggio.BikeSherpa.Backend.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Ggio.BikeSherpa.Backend.Infrastructure.Migrations
 {
     [DbContext(typeof(BackendDbContext))]
-    partial class BackendDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260130100653_UpdateCourierEntity")]
+    partial class UpdateCourierEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -103,9 +106,6 @@ namespace Ggio.BikeSherpa.Backend.Infrastructure.Migrations
 
                     b.Property<DateTimeOffset>("UpdatedAt")
                         .HasColumnType("timestamp with time zone");
-
-                    b.Property<string>("VatNumber")
-                        .HasColumnType("text");
 
                     b.HasKey("Id");
 
