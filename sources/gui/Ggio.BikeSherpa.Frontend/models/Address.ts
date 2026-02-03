d84f6b8cbd8da8123fd529d1a718fdeb42d770cf
francois_popieul <francois.popieul@hotmail.fr>
2026-02-03 14:39:40 +0100

    Removed useless spaces

--- sources/gui/Ggio.BikeSherpa.Frontend/models/Address.ts
+++ sources/gui/Ggio.BikeSherpa.Frontend/models/Address.ts
@@ -3,7 +3,7 @@ import * as zod from 'zod';
 export type Address = {
     name: string;
     fullAddress: string;
-    streetInfo: string;    
+    streetInfo: string;
     complement?: string | null;
     postcode: string;
     city: string;
