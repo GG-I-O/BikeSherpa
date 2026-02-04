9bc5458efcfd39e5327d57b856eacc5e17ec4994
francois_popieul <francois.popieul@hotmail.fr>
2026-02-04 16:23:32 +0100

    Reworked the delivery aggregate

--- sources/core/Ggio.BikeSherpa.Backend.Infrastructure/Configuration/DeliveryConfiguration.cs
+++ sources/core/Ggio.BikeSherpa.Backend.Infrastructure/Configuration/DeliveryConfiguration.cs
@@ -8,6 +8,33 @@ public class DeliveryConfiguration : IEntityTypeConfiguration<Delivery>
 {
      public void Configure(EntityTypeBuilder<Delivery> builder)
      {
-          builder.HasKey(x => x.Id);
+          builder.HasKey(d => d.Id);
+          builder.Property(d => d.Status).HasConversion<int>().IsRequired();
+          builder.Property(d => d.Code).IsRequired();
+          builder.Property(d => d.CustomerId).IsRequired();
+          builder.Property(d => d.TotalPrice).IsRequired();
+          builder.Property(d => d.ReportId).IsRequired();
+          builder.Property(d => d.Packing).IsRequired();
+          builder.Property(d => d.Details).IsRequired();
+          builder.ToTable("Deliveries");
+          
+          builder.OwnsMany(d => d.Steps, steps =>
+          {
+               steps.WithOwner().HasForeignKey("DeliveryId");
+               steps.HasKey("Id");
+               steps.Property(s => s.Id).ValueGeneratedNever();
+               steps.Property(s => s.StepType).HasConversion<int>().IsRequired();
+               steps.Property(s => s.Order).IsRequired();
+               steps.Property(s => s.Address).IsRequired();
+               steps.Property(s => s.Price).IsRequired();
+               steps.Property(s => s.Distance).IsRequired();
+               steps.Property(s => s.CourierId).IsRequired();
+               steps.Property(s => s.Comment).IsRequired();
+               steps.Property(s => s.FilePaths).IsRequired();
+               steps.Property(s => s.ContractDate).IsRequired();
+               steps.Property(s => s.EstimatedDeliveryDate).IsRequired();
+               steps.Property(s => s.RealDeliveryDate).IsRequired();
+               steps.ToTable("DeliverySteps");
+          });
      }
 }
