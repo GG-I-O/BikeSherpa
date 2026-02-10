39a0ac3669f72f603f63ceb163510fc0040126f9
francois_popieul <francois.popieul@hotmail.fr>
2026-02-10 17:05:52 +0100

    Fixed naming error

--- sources/core/Ggio.BikeSherpa.Backend/Features/Couriers/GetAll/GetAllCouriersHandler.cs
+++ sources/core/Ggio.BikeSherpa.Backend/Features/Couriers/GetAll/GetAllCouriersHandler.cs
@@ -6,15 +6,15 @@ using Mediator;
 
 namespace Ggio.BikeSherpa.Backend.Features.Couriers.GetAll;
 
-public record GetAllCouriersQuery(DateTimeOffset? lastSync): IQuery<List<Model.CourierCrud>>;
+public record GetAllCouriersQuery(DateTimeOffset? LastSync) : IQuery<List<Model.CourierCrud>>;
 
-public class GetAllCouriersHandler(IReadRepository<Courier> repository): IQueryHandler<GetAllCouriersQuery, List<Model.CourierCrud>>
+public class GetAllCouriersHandler(IReadRepository<Courier> repository) : IQueryHandler<GetAllCouriersQuery, List<Model.CourierCrud>>
 {
      public async ValueTask<List<Model.CourierCrud>> Handle(GetAllCouriersQuery query, CancellationToken cancellationToken)
      {
-          var allCouriers = query.lastSync is null ?
+          var allCouriers = query.LastSync is null ?
                (await repository.ListAsync(cancellationToken)).SelectFacets<Courier, Model.CourierCrud>() :
-               (await repository.ListAsync(new CourierByUpdatedAtSpecification(query.lastSync!.Value) ,cancellationToken)).SelectFacets<Courier, Model.CourierCrud>();
+               (await repository.ListAsync(new CourierByUpdatedAtSpecification(query.LastSync!.Value), cancellationToken)).SelectFacets<Courier, Model.CourierCrud>();
           return allCouriers.ToList();
      }
 }
