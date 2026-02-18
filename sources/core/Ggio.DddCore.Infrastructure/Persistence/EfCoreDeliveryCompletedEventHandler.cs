72590a03c4875df235c8d82c0573ab1adc907c4b
francois_popieul <francois.popieul@hotmail.fr>
2026-02-18 15:32:17 +0100

    feature: added event handlers for DeliveryStartedEvent, DeliveryCompletedEvent and DeliveryCancelledEvent

---
+++ sources/core/Ggio.DddCore.Infrastructure/Persistence/EfCoreDeliveryCompletedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using Mediator;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ggio.DddCore.Infrastructure.Persistence;
+
+public class EfCoreDeliveryCompletedEventHandler(DbContext dbContext) : INotificationHandler<DeliveryCompletedEvent>
+{
+     public async ValueTask Handle(DeliveryCompletedEvent notification, CancellationToken cancellationToken)
+     {
+          await dbContext.AddAsync(notification.NewEntity, cancellationToken);
+     }
+}
---
+++ sources/core/Ggio.DddCore.Infrastructure/Persistence/EfCoreDeliveryStartedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using Mediator;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ggio.DddCore.Infrastructure.Persistence;
+
+public class EfCoreDeliveryStartedEventHandler(DbContext dbContext) : INotificationHandler<DeliveryStartedEvent>
+{
+     public async ValueTask Handle(DeliveryStartedEvent notification, CancellationToken cancellationToken)
+     {
+          await dbContext.AddAsync(notification.NewEntity, cancellationToken);
+     }
+}
