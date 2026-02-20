12a8170b300a3e17bd0afed6790ee22b51d50263
francois_popieul <francois.popieul@hotmail.fr>
2026-02-20 14:22:15 +0100

    Refactored Delivery aggregate to use a status machine and mediator for event publishing; updated related event classes and tests

--- sources/core/tests/BackendTests/Features/Deliveries/GetAll/GetAllDeliveriesHandlerTests.cs
+++ sources/core/tests/BackendTests/Features/Deliveries/GetAll/GetAllDeliveriesHandlerTests.cs
@@ -3,6 +3,7 @@ using AutoFixture.AutoMoq;
 using Ggio.BikeSherpa.Backend.Domain.DeliveryAggregate;
 using Ggio.BikeSherpa.Backend.Features.Deliveries.GetAll;
 using Ggio.DddCore;
+using Mediator;
 using Moq;
 
 namespace BackendTests.Features.Deliveries.GetAll;
