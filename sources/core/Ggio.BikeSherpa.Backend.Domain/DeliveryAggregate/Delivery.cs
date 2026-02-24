b3f761dfbd1df2534973a4ad1c9d801681f91dc0
francois_popieul <francois.popieul@hotmail.fr>
2026-02-24 10:15:14 +0100

    Refactored PricingStrategyServiceTests: Changed static fields to readonly and improved variable naming for clarity; removed unnecessary using directive in Delivery.cs and  AddDeliveryHandler.cs

--- sources/core/Ggio.BikeSherpa.Backend.Domain/DeliveryAggregate/Delivery.cs
+++ sources/core/Ggio.BikeSherpa.Backend.Domain/DeliveryAggregate/Delivery.cs
@@ -1,4 +1,3 @@
-using System.Runtime.InteropServices.JavaScript;
 using Ggio.BikeSherpa.Backend.Domain.CustomerAggregate;
 using Ggio.BikeSherpa.Backend.Domain.DeliveryAggregate.Enumerations;
 using Ggio.BikeSherpa.Backend.Domain.DeliveryAggregate.Events;
