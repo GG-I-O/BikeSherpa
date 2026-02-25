d9128495f6b9bcef814e5e5a8488ba3f71ec8ff2
francois_popieul <francois.popieul@hotmail.fr>
2026-02-25 16:02:46 +0100

    Add CancelDelivery command and endpoint with validation

---
+++ sources/core/Ggio.BikeSherpa.Backend/Features/Deliveries/Update/CancelDeliveryEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using FastEndpoints;
+using Ggio.BikeSherpa.Backend.Extensions;
+using Mediator;
+using Microsoft.AspNetCore.Http;
+
+namespace Ggio.BikeSherpa.Backend.Features.Deliveries.Update;
+
+public class CancelDeliveryEndpoint(IMediator mediator) : EndpointWithoutRequest
+{
+     public override void Configure()
+     {
+          Put("/delivery/{deliveryId:guid}/cancel");
+          Policies("write:deliveries");
+          Description(x => x.WithTags("delivery"));
+     }
+
+     public override async Task HandleAsync(CancellationToken ct)
+     {
+          var command = new CancelDeliveryCommand(
+               Route<Guid>("deliveryId")
+          );
+
+          var result = await mediator.Send(command, ct);
+          await Send.ToEndpointResult(result, ct);
+     }
+}
