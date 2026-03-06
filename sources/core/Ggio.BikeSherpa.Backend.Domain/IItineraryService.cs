bdaaa25b4c685df75bf349159a18fec1843c4bdc
francois_popieul <francois.popieul@hotmail.fr>
2026-03-06 16:19:38 +0100

    feature: Added Itinerary API integration with Refit
    - Created IItineraryApi interface for itinerary and isochrone services.
    - Implemented ItineraryService to handle itinerary requests.
    - Added Refitter configuration file for generating API client from OpenAPI spec.
    - Included OpenAPI specification (itineraire.yaml) for the itinerary service.
    - Updated project dependencies to include Refit and Refit.HttpClientFactory.
    - Enhanced appsettings.json to include base URL for ItineraryService.
    - Updated readme.md with instructions for using Refitter to generate API clients.

---
+++ sources/core/Ggio.BikeSherpa.Backend.Domain/IItineraryService.cs
@@ -0,0 +1,12 @@
+﻿namespace Ggio.BikeSherpa.Backend.Domain;
+
+public record ItineraryResult(double DistanceKm, double TimeMinutes);
+
+public interface IItineraryService
+{
+     Task<ItineraryResult> GetItineraryInfoAsync(
+          string startStepCoordinates,
+          string endStepCoordinates,
+          CancellationToken cancellationToken = default);
+}
+
--- sources/core/Ggio.BikeSherpa.Backend.Infrastructure/Bootstrap.cs
+++ sources/core/Ggio.BikeSherpa.Backend.Infrastructure/Bootstrap.cs
@@ -1,10 +1,13 @@
+using Ggio.BikeSherpa.Backend.Domain;
 using Ggio.BikeSherpa.Backend.Domain.DeliveryAggregate.Services.Repositories;
+using Ggio.BikeSherpa.Backend.Infrastructure.GeoService;
 using Ggio.BikeSherpa.Backend.Infrastructure.Interceptors;
 using Ggio.BikeSherpa.Backend.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ggio.DddCore.Infrastructure;
+using Refit;
 
 namespace Ggio.BikeSherpa.Backend.Infrastructure;
 
@@ -19,6 +22,12 @@ public static class Bootstrap
                     options.UseNpgsql(connectionString)
                          .AddInterceptors(new DateInterceptor())
                );
+               services.AddRefitClient<IItineraryApi>()
+                    .ConfigureHttpClient(c =>
+                    {
+                         c.BaseAddress = new Uri(configuration["ItineraryService:BaseUrl"]!);
+                    });
+               services.AddScoped<IItineraryService, ItineraryService>();
                services.AddScoped<IDeliveryZoneRepository, DeliveryZoneRepository>();
                services.AddScoped<IPackingSizeRepository, PackingSizeRepository>();
                services.AddScoped<IUrgencyRepository, UrgencyRepository>();
