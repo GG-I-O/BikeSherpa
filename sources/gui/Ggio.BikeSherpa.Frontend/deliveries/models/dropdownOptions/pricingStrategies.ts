ea1a564534dedcf6fe9924444eadcd2d202cd590
antoine <a.epiard.101010@gmail.com>
2026-04-02 17:22:25 +0200

    fix: forgot AGAIN to commit some files created outside of rider...

---
+++ sources/gui/Ggio.BikeSherpa.Frontend/deliveries/models/dropdownOptions/pricingStrategies.ts
@@ -0,0 +1,7 @@
+const pricingStrategies = [
+    { label: "Personnalisé", value: "0" },
+    { label: "Course", value: "1" },
+    { label: "Tournée", value: "2" }
+];
+
+export default pricingStrategies;
---
+++ sources/gui/Ggio.BikeSherpa.Frontend/deliveries/models/dropdownOptions/urgencies.ts
@@ -0,0 +1,7 @@
+const urgencies = [
+    { label: "Eco", value: "Eco" },
+    { label: "Standard", value: "Standard" },
+    { label: "Urgent", value: "Urgent" }
+]
+
+export default urgencies;
