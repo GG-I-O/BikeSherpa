c4a79fe64cbacce83784f62396729c2a5cc47bf5
antoine <a.epiard.101010@gmail.com>
2026-04-13 11:47:59 +0200

    test: PricingStrategies endpoint tests

---
+++ sources/core/tests/BackendTests/Features/Public/PricingStrategies/GetAll/GetAllPricingStrategiesEndpointTests.cs
@@ -0,0 +1,94 @@
+using System.Net;
+using System.Text.Json;
+using AutoFixture;
+using AwesomeAssertions;
+using BackendTests.Services;
+using Ggio.BikeSherpa.Backend.Features.Public.PricingStrategies.GetAll;
+using Ggio.BikeSherpa.Backend.Features.Public.PricingStrategies.Model;
+using JetBrains.Annotations;
+using Mediator;
+using Moq;
+
+namespace BackendTests.Features.Public.PricingStrategies.GetAll;
+
+[UsedImplicitly]
+public class GetAllPricingStrategiesWebApplicationFactory() : TestWebApplicationFactory();
+
+public class GetAllPricingStrategiesEndpointTests(
+     GetAllPricingStrategiesWebApplicationFactory factory,
+     ITestContextAccessor testContextAccessor
+     ): IClassFixture<GetAllPricingStrategiesWebApplicationFactory>
+{
+     private readonly HttpClient _client = factory.CreateClient();
+     private readonly Mock<IMediator> _mockMediator = factory.MockMediator;
+     private readonly Fixture _fixture = new();
+     private readonly CancellationToken _cancellationToken = testContextAccessor.Current.CancellationToken;
+
+     private readonly JsonSerializerOptions _jsonSerializerOptions = new()
+     {
+          PropertyNameCaseInsensitive = false,
+          PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+     };
+     
+     [Fact]
+     public async Task HandleAsync_ShouldCallMediatorAndSendResponse_WhenPricingStrategiesExist()
+     {
+          // Arrange
+          _mockMediator.Reset();
+          var expectedPricingStrategies = _fixture.Create<List<PricingStrategyDto>>();
+
+          _mockMediator.Setup(m => m.Send(
+                    It.IsAny<GetAllPricingStrategiesQuery>(),
+                    It.IsAny<CancellationToken>()))
+               .ReturnsAsync(expectedPricingStrategies);
+
+          // Act
+          var response = await _client.GetAsync("/api/public/pricingStrategies", _cancellationToken);
+
+          // Assert
+          response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+          _mockMediator.Verify(
+               m => m.Send(
+                    It.IsAny<GetAllPricingStrategiesQuery>(),
+                    It.IsAny<CancellationToken>()
+               ),
+               Times.Once
+          );
+
+          var responseBody = await response.Content.ReadAsStringAsync(_cancellationToken);
+          var responseArray = JsonSerializer.Deserialize<JsonElement>(responseBody, _jsonSerializerOptions);
+
+          responseArray.GetArrayLength().Should().Be(expectedPricingStrategies.Count);
+     }
+
+     [Fact]
+     public async Task HandleAsync_ShouldCallMediatorAndSendResponse_WhenNoUrgenciesExist()
+     {
+          // Arrange
+          _mockMediator.Reset();
+          _mockMediator.Setup(m => m.Send(
+                    It.IsAny<GetAllPricingStrategiesQuery>(),
+                    It.IsAny<CancellationToken>()))
+               .ReturnsAsync([]);
+
+          // Act
+          var response = await _client.GetAsync("/api/public/pricingStrategies", _cancellationToken);
+
+          // Assert
+          response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+          _mockMediator.Verify(
+               m => m.Send(
+                    It.IsAny<GetAllPricingStrategiesQuery>(),
+                    It.IsAny<CancellationToken>()
+               ),
+               Times.Once
+          );
+
+          var responseBody = await response.Content.ReadAsStringAsync(_cancellationToken);
+          var responseArray = JsonSerializer.Deserialize<JsonElement>(responseBody, _jsonSerializerOptions);
+
+          responseArray.GetArrayLength().Should().Be(0);
+     }
+}
---
+++ sources/core/tests/BackendTests/Features/Public/PricingStrategies/GetAll/GetAllPricingStrategiesHandlerTests.cs
@@ -0,0 +1,31 @@
+using AwesomeAssertions;
+using Ggio.BikeSherpa.Backend.Domain.DeliveryAggregate.Enumerations;
+using Ggio.BikeSherpa.Backend.Features.Public.PricingStrategies.GetAll;
+
+namespace BackendTests.Features.Public.PricingStrategies.GetAll;
+
+public class GetAllPricingStrategiesHandlerTests
+{
+     [Fact]
+     public async Task Handle_ShouldReturnAllPricingStrategies_InTheEnum()
+     {
+          // Arrange
+          var sut = CreateSut();
+          var query = new GetAllPricingStrategiesQuery();
+          
+          // Act
+          var result = await sut.Handle(query, CancellationToken.None);
+          
+          // Assert
+          result.Should().NotBeNull();
+          result.Count.Should().Be(3);
+          result[(int)PricingStrategy.CustomStrategy].Value.Should().Be(PricingStrategy.CustomStrategy);
+          result[(int)PricingStrategy.SimpleDeliveryStrategy].Value.Should().Be(PricingStrategy.SimpleDeliveryStrategy);
+          result[(int)PricingStrategy.TourDeliveryStrategy].Value.Should().Be(PricingStrategy.TourDeliveryStrategy);
+     }
+     
+     private GetAllPricingStrategiesHandler CreateSut()
+     {
+          return new GetAllPricingStrategiesHandler();
+     }
+}
