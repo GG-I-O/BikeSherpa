4e93e5f32ddf5b2777b468ab33c89547a57d7b10
antoine <a.epiard.101010@gmail.com>
2026-04-30 11:11:50 +0200

    fix: removed unused method

--- sources/core/Ggio.BikeSherpa.Backend/Features/Deliveries/Model/DeliveryStepCrudMapper.cs
+++ sources/core/Ggio.BikeSherpa.Backend/Features/Deliveries/Model/DeliveryStepCrudMapper.cs
@@ -3,10 +3,8 @@ using Ggio.BikeSherpa.Backend.Domain.DeliveryAggregate;
 
 namespace Ggio.BikeSherpa.Backend.Features.Deliveries.Model;
 
-public class DeliveryStepCrudMapper : IFacetMapConfiguration<DeliveryStep, DeliveryStepCrud>, IFacetMapConfiguration<DeliveryStepCrud, DeliveryStep>
+public class DeliveryStepCrudMapper : IFacetMapConfiguration<DeliveryStepCrud, DeliveryStep>
 {
-     public static void Map(DeliveryStep source, DeliveryStepCrud target){}
-
      public static void Map(DeliveryStepCrud source, DeliveryStep target)
      {
           target.Completed = source.Completed;
