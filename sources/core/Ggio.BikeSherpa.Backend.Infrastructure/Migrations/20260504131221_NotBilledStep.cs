72f179527eab09a618889c3ff99cf7184a42ed50
antoine <a.epiard.101010@gmail.com>
2026-05-04 15:49:24 +0200

    feat: noBilled migration

---
+++ sources/core/Ggio.BikeSherpa.Backend.Infrastructure/Migrations/20260504131221_NotBilledStep.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ggio.BikeSherpa.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotBilledStep : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NotBilled",
+                table: "DeliverySteps",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotBilled",
+                table: "DeliverySteps");
+        }
+    }
+}
