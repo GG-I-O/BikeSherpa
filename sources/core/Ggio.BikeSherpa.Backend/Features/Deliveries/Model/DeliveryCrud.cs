971dc47de2436d28eda23c7749b5828d10b6545a
antoine <a.epiard.101010@gmail.com>
2026-05-20 10:27:52 +0200

    feat: remapping needed from Model to Crud data model

--- sources/core/Ggio.BikeSherpa.Backend/Features/Deliveries/Model/DeliveryCrud.cs
+++ sources/core/Ggio.BikeSherpa.Backend/Features/Deliveries/Model/DeliveryCrud.cs
@@ -3,10 +3,16 @@ using Ggio.BikeSherpa.Backend.Domain.DeliveryAggregate;
 
 namespace Ggio.BikeSherpa.Backend.Features.Deliveries.Model;
 
-[Facet(typeof(Delivery), exclude: [nameof(Delivery.DomainEvents), nameof(Delivery.Steps), nameof(Delivery.Urgency)], Configuration = typeof(DeliveryCrudMapper))]
+[Facet(typeof(Delivery), exclude: [
+     nameof(Delivery.DomainEvents),
+     nameof(Delivery.Steps),
+     nameof(Delivery.Urgency),
+     nameof(Delivery.PackingSize)
+], Configuration = typeof(DeliveryCrudMapper))]
 public partial record DeliveryCrud
 {
      public List<DeliveryStepDto> Steps { get; set; } = [];
-     public string Urgency { get; set; }
+     public required string Urgency { get; set; }
+     public required string PackingSize { get; set; }
      public DateTimeOffset? LimitDate { get; set; }
 }
