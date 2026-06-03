ab57141688658840e0eba6a739006cdc0519e8b9
antoine <a.epiard.101010@gmail.com>
2026-06-03 09:35:44 +0200

    feat: AllowAnonymous on StaticData endpoints

--- sources/core/Ggio.BikeSherpa.Backend/Features/StaticData/Urgencies/GetAll/GetAllUrgenciesEndpoint.cs
+++ sources/core/Ggio.BikeSherpa.Backend/Features/StaticData/Urgencies/GetAll/GetAllUrgenciesEndpoint.cs
@@ -10,8 +10,8 @@ public class GetAllUrgenciesEndpoint(IMediator mediator) : EndpointWithoutReques
      public override void Configure()
      {
           Get("/general/urgencies");
-          Policies("AuthenticatedUser");
           Description(x => x.WithTags("general"));
+          AllowAnonymous();
      }
 
      public override async Task HandleAsync(CancellationToken ct)
