b37c5ca16f26972faccc0bb32e95711c8a987ef1
Ludovic Godrant <ludovic.godrant@gg-io.com>
2026-06-05 09:41:06 +0200

    Warning résolutions

--- sources/core/Ggio.BikeSherpa.Backend/Features/Deliveries/Model/DeliveryCrud.cs
+++ sources/core/Ggio.BikeSherpa.Backend/Features/Deliveries/Model/DeliveryCrud.cs
@@ -3,16 +3,11 @@ using Ggio.BikeSherpa.Backend.Domain.DeliveryAggregate;
 
 namespace Ggio.BikeSherpa.Backend.Features.Deliveries.Model;
 
-[Facet(typeof(Delivery), exclude: [
-     nameof(Delivery.DomainEvents),
-     nameof(Delivery.Steps),
-     nameof(Delivery.Urgency),
-     nameof(Delivery.PackingSize)
-], Configuration = typeof(DeliveryCrudMapper))]
+[Facet(typeof(Delivery), nameof(Delivery.DomainEvents), nameof(Delivery.Steps), nameof(Delivery.Urgency), nameof(Delivery.PackingSize), Configuration = typeof(DeliveryCrudMapper))]
 public partial record DeliveryCrud
 {
      public List<DeliveryStepDto> Steps { get; set; } = [];
-     public string Urgency { get; set; }
-     public string PackingSize { get; set; }
+     public string Urgency { get; set; } = string.Empty;
+     public string PackingSize { get; set; } = string.Empty;
      public DateTimeOffset? LimitDate { get; set; }
 }
