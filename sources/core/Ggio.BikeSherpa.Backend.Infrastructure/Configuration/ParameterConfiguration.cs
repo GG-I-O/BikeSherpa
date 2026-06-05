78c4a92782c3e450d689bc000f580d6101f026fc
antoine <a.epiard.101010@gmail.com>
2026-06-05 13:58:48 +0200

    fix: broken migrations

--- sources/core/Ggio.BikeSherpa.Backend.Infrastructure/Configuration/ParameterConfiguration.cs
+++ sources/core/Ggio.BikeSherpa.Backend.Infrastructure/Configuration/ParameterConfiguration.cs
@@ -32,38 +32,27 @@ public class ParameterConfiguration : IEntityTypeConfiguration<Parameter>
               {
                    Key = ParameterRepository.WorkEndDate,
                    Value = new DateTimeOffset(1,1,1,19,0,0,0,new TimeSpan(0)).ToString("yyyy-MM-ddTHH:mm:ss")
+              },
+              new
+              {
+                   Key = ParameterRepository.SimpleDeliveryMailTemplateKey,
+                   Value = "Your delivery is ready for pickup at {pickupLocation} on {pickupDate}. {deliverycode} { pickupaddress} { destinationaddress} {PickupDate} {LoadingSlot} "
+              },
+              new
+              {
+                   Key = ParameterRepository.SimpleDeliveryMailSubjectKey,
+                   Value = "Delivery Ready for Pickup"
+              },
+              new
+              {
+                   Key = ParameterRepository.TourDeliveryMailTemplateKey,
+                   Value = "Your delivery is ready for pickup at {pickupLocation} on {pickupDate}. {deliverycode} { pickupaddress} { destinationaddress} {PickupDate} {LoadingSlot} "
+              },
+              new
+              {
+                   Key = ParameterRepository.TourDeliveryMailSubjectKey,
+                   Value = "Tour delivery Ready for Pickup"
               }
          );
-          builder.HasKey(p => p.Key);
-          builder.ToTable("Parameters");
-          builder.Property(p => p.Key).HasMaxLength(100).IsRequired();
-          builder.Property(p => p.Value).IsRequired();
-          builder.HasData(
-               new
-               {
-                    Key = ParameterRepository.VatRateKey,
-                    Value = "20"
-               },
-               new
-               {
-                    Key = ParameterRepository.SimpleDeliveryMailTemplateKey,
-                    Value = "Your delivery is ready for pickup at {pickupLocation} on {pickupDate}. {deliverycode} { pickupaddress} { destinationaddress} {PickupDate} {LoadingSlot} "
-               },
-               new
-               {
-                    Key = ParameterRepository.SimpleDeliveryMailSubjectKey,
-                    Value = "Delivery Ready for Pickup"
-               },
-               new
-               {
-                    Key = ParameterRepository.TourDeliveryMailTemplateKey,
-                    Value = "Your delivery is ready for pickup at {pickupLocation} on {pickupDate}. {deliverycode} { pickupaddress} { destinationaddress} {PickupDate} {LoadingSlot} "
-               },
-               new
-               {
-                    Key = ParameterRepository.TourDeliveryMailSubjectKey,
-                    Value = "Tour delivery Ready for Pickup"
-               }
-          );
      }
 }
