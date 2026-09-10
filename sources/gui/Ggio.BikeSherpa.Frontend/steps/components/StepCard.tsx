8f681700c82e98b740adfbeaf721e1b80ecee779
antoine <a.epiard.101010@gmail.com>
2026-09-10 10:47:58 +0200

    Adds a "check" for completed steps

--- sources/gui/Ggio.BikeSherpa.Frontend/steps/components/StepCard.tsx
+++ sources/gui/Ggio.BikeSherpa.Frontend/steps/components/StepCard.tsx
@@ -1,11 +1,12 @@
-import { Dimensions, Pressable, View } from "react-native";
-import { Button, Card, Divider, Modal, Portal, Text, useTheme } from "react-native-paper";
+import {Dimensions, Pressable, View} from "react-native";
+import {Button, Card, Divider, Modal, Portal, Text, useTheme} from "react-native-paper";
 import DeliveryTypeIcon from "@/deliveries/components/DeliveryTypeIcon";
-import { useState } from "react";
-import { IOCContainer } from "@/bootstrapper/constants/IOCContainer";
-import { IAddressService } from "@/spi/AddressSPI";
-import { ServicesIdentifiers } from "@/bootstrapper/constants/ServicesIdentifiers";
+import {useState} from "react";
+import {IOCContainer} from "@/bootstrapper/constants/IOCContainer";
+import {IAddressService} from "@/spi/AddressSPI";
+import {ServicesIdentifiers} from "@/bootstrapper/constants/ServicesIdentifiers";
 import {StepToDisplay} from "@/steps/models/StepToDisplay";
+import {Icon} from "react-native-paper/src";
 
 type Props = {
     step: StepToDisplay,
@@ -13,7 +14,7 @@ type Props = {
     isSelected?: boolean
 }
 
-export default function StepCard({ step, onPress, isSelected = false }: Props) {
+export default function StepCard({step, onPress, isSelected = false}: Props) {
     const theme = useTheme();
 
     const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
@@ -34,19 +35,25 @@ export default function StepCard({ step, onPress, isSelected = false }: Props) {
                 }}
             >
                 <Card.Content>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                        <DeliveryTypeIcon type={step.type} />
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingLeft: 18}}>
+                        <DeliveryTypeIcon type={step.type}/>
                         <Text>{step.deliveryCode}</Text>
-                        <Text style={{ fontWeight: "bold"}}>{step.estimatedTime}</Text>
+                        <Text style={{fontWeight: "bold"}}>{step.estimatedTime}</Text>
+                        {step.completed &&
+                            <View style={{position: 'absolute', left: 0}}>
+                                <Icon size={20} source={"check-circle"} color={"#60cc70"}/>
+                            </View>
+                        }
                     </View>
-                    <Divider />
+                    <Divider/>
                     <Pressable
-                        style={{ justifyContent: 'space-evenly', marginInline: 8, marginTop: 4 }}
+                        style={{justifyContent: 'space-evenly', marginInline: 8, marginTop: 4}}
                         onPress={() => {
                             setIsModalVisible(true);
                         }}
                     >
-                        <Text style={{textAlign: 'right', fontStyle: 'italic', marginLeft: 24}} numberOfLines={1}>{step.address.name}</Text>
+                        <Text style={{textAlign: 'right', fontStyle: 'italic', marginLeft: 24}}
+                              numberOfLines={1}>{step.address.name}</Text>
                         <Text>{step.address.streetInfo}</Text>
                         <Text>{`${step.address.postcode} ${step.address.city}`}</Text>
                     </Pressable>
@@ -57,13 +64,19 @@ export default function StepCard({ step, onPress, isSelected = false }: Props) {
                 <Modal
                     visible={isModalVisible}
                     onDismiss={() => setIsModalVisible(false)}
-                    contentContainerStyle={{ backgroundColor: theme.colors.background, padding: 32, gap: 16, justifyContent: 'center', alignItems: 'center' }}
+                    contentContainerStyle={{
+                        backgroundColor: theme.colors.background,
+                        padding: 32,
+                        gap: 16,
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
                 >
                     <Text style={{}}>
-                        Ouvrir la carte pour cette adresse ?
+                        Ouvrir la carte pour cette adresse ?
                     </Text>
 
-                    <View style={{ flexDirection: 'row', gap: 8 }}>
+                    <View style={{flexDirection: 'row', gap: 8}}>
                         <Button
                             mode="outlined"
                             onPress={() => {
