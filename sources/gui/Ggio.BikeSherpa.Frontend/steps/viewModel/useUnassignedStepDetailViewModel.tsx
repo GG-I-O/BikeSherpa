4eba027c1541cb895302b0c74a93d1c864974a9e
antoine <a.epiard.101010@gmail.com>
2026-09-17 14:46:31 +0200

    fix: missed rename

--- sources/gui/Ggio.BikeSherpa.Frontend/steps/viewModel/useUnassignedStepDetailViewModel.tsx
+++ sources/gui/Ggio.BikeSherpa.Frontend/steps/viewModel/useUnassignedStepDetailViewModel.tsx
@@ -2,13 +2,13 @@ import {IOCContainer} from "@/bootstrapper/constants/IOCContainer";
 import {IStepServices} from "@/steps/spi/IStepServices";
 import {StepServiceIdentifier} from "@/steps/bootstrapper/StepServiceIdentifier";
 import UnassignedStepDetailViewModel from "@/steps/viewModel/UnassignedStepDetailViewModel";
-import {ServicesIdentifiers} from "@/bootstrapper/constants/ServicesIdentifiers";
 import {IUserService} from "@/spi/AuthSPI";
 import {useEffect, useState} from "react";
+import {AuthServiceIdentifier} from "@/infra/auth/bootstrapper/AuthServiceIdentifier";
 
 export default function useUnassignedStepDetailViewModel() {
     const stepServices = IOCContainer.get<IStepServices>(StepServiceIdentifier.Services);
-    const userService = IOCContainer.get<IUserService>(ServicesIdentifiers.UserService);
+    const userService = IOCContainer.get<IUserService>(AuthServiceIdentifier.UserService);
 
     const viewModel = new UnassignedStepDetailViewModel(stepServices);
     
